3411120e0fbcb9bcf8f8c608b14261772294ff82
Normanfamdamly <104736546+Normanfamdamly@users.noreply.github.com>
2022-09-04 15:01:47 -0400

    fixed

--- scraping.py
+++ scraping.py
@@ -1,6 +1,4 @@
 # Import Splinter and BeautifulSoup
-from nturl2path import url2pathname
-from types import NoneType
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
@@ -9,42 +7,41 @@ import pandas as pd
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
-def mars_news(browser):
-
 # Scrape Mars News
-# Visit the mars nasa news site
+def mars_news(browser):
+    # Visit the mars nasa news site
     url = 'https://redplanetscience.com/'
     browser.visit(url)
 
-# Optional delay for loading the page
-browser.is_element_present_by_css('div.list_text', wait_time=1)
+    # OPtional delay for loading page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
 
-# Convert the browser html to a soup object and then quit the browser
-html = browser.html
-news_soup = soup(html, 'html.parser')
+    # Convert the browser html to a soup object and then quit the browser
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
 
-# Add try/except for error handling
-try:
-   slide_elem = news_soup.select_one('div.list_text')
-   # Use the parent element to find the first 'a' tag and save it as 'news_title'
-   news_title = slide_elem.find('div', class_='content_title').get_text()
-   # Use the parent element to find the paragraph text
-   news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+    # Add try/except for error handling
+    try:
+        slide_elem = news_soup.select_one('div.list_text')
+        # Use the parent element to find the first 'a' tag and save it as 'news_title'
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
 
-except AttributeError:
-    return None, None
+    except AttributeError:
+        return None, None
     
-return news_title, news_p
+    return news_title, news_p
 
 
-# Use the parent element to find the first `a` tag and save it as `news_title`
-news_title = slide_elem.find('div', class_='content_title').get_text()
-news_title
+    # Use the parent element to find the first `a` tag and save it as `news_title`
+    news_title = slide_elem.find('div', class_='content_title').get_text()
+    news_title
 
 
-# Use the parent element to find the paragraph text
-news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-news_p
+    # Use the parent element to find the paragraph text
+    news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+    news_p
 
 
 # ### Featured Images
@@ -54,26 +51,26 @@ def featured_image(browser):
     url = 'https://spaceimages-mars.com'
     browser.visit(url)
 
-# Find and click the full image button
-full_image_elem = browser.find_by_tag('button')[1]
-full_image_elem.click()
+    # Find and click the full image button
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
 
-# Parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
 
-# Add try/except for erro handling
-try: 
-    # Find the relative image url
-    img_url_rel = img_soup.find('img', class_='fancybox-image').get('src') 
-except AttributeError:
-    return None
+    # Add try/except for erro handling
+    try: 
+        # Find the relative image url
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src') 
+    except AttributeError:
+        return None
 
-# Use the base url to create an absolute url
-img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-img_url
+    # Use the base url to create an absolute url
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+    img_url
 
-return img_url
+    return img_url
 
 def mars_facts():
     # Add try/except for error handling
@@ -83,12 +80,12 @@ def mars_facts():
     except BaseException:
         return None
 
-# Assign columns and set index to dataframe
-df.columns=['description', 'Mars', 'Earth']
-df.set_index('description', inplace=True)
+    # Assign columns and set index to dataframe
+    df.columns=['description', 'Mars', 'Earth']
+    df.set_index('description', inplace=True)
 
-# Convert dataframe into HTML format, add bootstrap
-return df.to_html()
+    # Convert dataframe into HTML format, add bootstrap
+    return df.to_html()
 
 browser.quit()
 
