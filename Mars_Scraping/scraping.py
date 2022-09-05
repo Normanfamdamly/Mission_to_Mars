90ba176b9045f22ae6101a7f70651cc0fd313724
Normanfamdamly <104736546+Normanfamdamly@users.noreply.github.com>
2022-09-05 13:11:51 -0400

    identical

--- Mars_Scraping/scraping.py
+++ Mars_Scraping/scraping.py
@@ -1,10 +1,9 @@
-# Import Splinter and BeautifulSoup
+# Import Splinter, BeautifulSoup, and Pandas
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-import requests as requests
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import datetime as dt
+
 
 def scrape_all():
     # Initiate headless driver for deployment
@@ -33,7 +32,7 @@ def mars_news(browser):
     url = 'https://redplanetscience.com/'
     browser.visit(url)
 
-    # Optional delay for loading page
+    # Optional delay for loading the page
     browser.is_element_present_by_css('div.list_text', wait_time=1)
 
     # Convert the browser html to a soup object and then quit the browser
@@ -44,18 +43,17 @@ def mars_news(browser):
     try:
         slide_elem = news_soup.select_one('div.list_text')
         # Use the parent element to find the first 'a' tag and save it as 'news_title'
-        news_title = slide_elem.find('div', class_='content_title').get_text()
+        news_title = slide_elem.find("div", class_="content_title").get_text()
         # Use the parent element to find the paragraph text
-        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+        news_p = slide_elem.find("div", class_="article_teaser_body").get_text()
 
     except AttributeError:
         return None, None
-    
+
     return news_title, news_p
 
- # ### Featured Images
-def featured_image(browser):
 
+def featured_image(browser):
     # Visit URL
     url = 'https://spaceimages-mars.com'
     browser.visit(url)
@@ -68,29 +66,32 @@ def featured_image(browser):
     html = browser.html
     img_soup = soup(html, 'html.parser')
 
-    # Add try/except for erro handling
-    try: 
-        # Find the relative image url
-        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src') 
+    # Add try/except for error handling
+    try:
+        # find the relative image url
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+
     except AttributeError:
         return None
 
     # Use the base url to create an absolute url
     img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-   
+
     return img_url
+
 
 def mars_facts():
     # Add try/except for error handling
     try:
-    # USe 'read_html to scrape the facts table into the dataframe    
+        # Use 'read_html' to scrape the facts table into a dataframe
         df = pd.read_html('https://galaxyfacts-mars.com')[0]
+
     except BaseException:
         return None
 
-    # Assign columns and set index to dataframe
-    df.columns=['description', 'Mars', 'Earth']
-    df.set_index('description', inplace=True)
+    # Assign columns and set index of dataframe
+    df.columns=['Description', 'Mars', 'Earth']
+    df.set_index('Description', inplace=True)
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
