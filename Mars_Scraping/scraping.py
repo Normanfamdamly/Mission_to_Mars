59499a3c7e02707d2849c34b2c356c984cad41dc
Normanfamdamly <104736546+Normanfamdamly@users.noreply.github.com>
2022-09-05 13:04:50 -0400

    updated

--- Mars_Scraping/scraping.py
+++ Mars_Scraping/scraping.py
@@ -1,26 +1,26 @@
 # Import Splinter and BeautifulSoup
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-import requests
+import requests as requests
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import datetime as dt
 
 def scrape_all():
-    # Initiate headless driver for deplaoyment
-    executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=True)
+    # Initiate headless driver for deployment
+    browser = Browser("chrome", executable_path="chromedriver", headless=True)
 
     news_title, news_paragraph = mars_news(browser)
 
-    # Run all scraping functions and store results in dictionary 
+    # Run all scraping functions and store results in a dictionary
     data = {
-         "news_title": news_title,
-         "news_paragraph": news_paragraph,
-         "featured_image": featured_image,
-         "facts": mars_facts(),
-         "last_modified": dt.datetime.now()
-         }
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+
     # Stop webdriver and return data
     browser.quit()
     return data
